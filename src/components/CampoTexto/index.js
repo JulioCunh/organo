9a87b571495a14d8dc6616a70fb0fafbfda1d5ad
JulioCunh <fcezarf@gmail.com>
2023-08-15 11:58:51 -0300

    3 commit

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,12 +1,21 @@
-import './style.css';
+import './styles.css';
 
 const CampoTexto = (props) => {
   const placeholderModificada = `${props.placeholder}...`;
 
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={placeholderModificada} />
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
     </div>
   );
 };
